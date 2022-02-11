5a58902743ec111a509b1f3f2ec0a04d9c97b8e1
cwf96991 <francis.choi@sonivy.com>
2022-02-11 16:21:54 +0800

    fix SEO

---
+++ src/pages/resume.js
@@ -0,0 +1,10 @@
+import About from "../templates/resume";
+const Index = () => <About />;
+// export function getStaticProps({ locale }) {
+//   return {
+//     props: {
+//       messages: require(`../locales/${locale}.json`),
+//     },
+//   };
+// }
+export default Index;

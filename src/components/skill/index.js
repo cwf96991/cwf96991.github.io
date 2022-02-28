c05731bd7100695b0469d7dbf8f235fd1fc0a866
cwf96991 <66364950+cwf96991@users.noreply.github.com>
2022-02-28 11:41:42 +0000

    Deploying to master from @ cwf96991/cwf96991.github.io@21ebc0091508f04e9e6f4986a6a61ba88de93b96 🚀

--- src/components/skill/index.js
+++
@@ -1,5 +0,0 @@
-import FormWidget from "./formWidget";
-import HoverCardWidget from "./hoverCard";
-import TestSkill from "./test";
-import SocialMediaSkill from "./socialMediaWidget"
-export { FormWidget, HoverCardWidget, TestSkill ,SocialMediaSkill};

d1449d46e988432aee469ae095252b863709ae53
cwf96991 <francis.choi@sonivy.com>
2022-02-28 18:00:07 +0800

    add hover card

---
+++ src/components/skill/index.js
@@ -0,0 +1,4 @@
+import FormWidget from "./formWidget";
+import HoverCardWidget from "./hoverCard";
+import TestSkill from "./test";
+export { FormWidget, HoverCardWidget, TestSkill };
---
+++ src/components/skill/test.js
@@ -0,0 +1,31 @@
+import { Header, SkillContainer, MobileDeskTopSection } from "./widget";
+
+const TestSkill = () => {
+  //   const Widget = () => {
+  //     return (
+  //     //   <LiquidSwipe
+  //     //     components={[<div className="bg-orange-300 w-full h-full" >1</div>, <div className="bg-red-300 w-full h-full">2</div>]}
+  //     //     style={{
+  //     //       height: "80vh",
+  //     //       width: "80vw",
+  //     //       margin: "10vh auto",
+  //     //       borderRadius: "20px",
+  //     //     }}
+  //     //   />
+  //     );
+  //   };
+
+  return (
+    <div className="">
+      {/* <Header title="Test | my work" header="Test" skill="test" />
+
+      <SkillContainer phone={<Widget />} window={<Widget />} />
+
+      <MobileDeskTopSection>
+        <Widget />
+      </MobileDeskTopSection> */}
+    </div>
+  );
+};
+
+export default TestSkill;

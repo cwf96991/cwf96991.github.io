75ce8ace650f74716a80a0d894990df59c217cbb
cwf96991 <66364950+cwf96991@users.noreply.github.com>
2022-04-11 15:35:43 +0000

    Deploying to master from @ cwf96991/cwf96991.github.io@7d2724ea757a8ebcd768a18642bacc6828c8d9fc 🚀

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import LabelList from "./labelList";
-import NewlineText from "./newlineText";
-import { OutlineBtn, BgBtn } from "./btn";
-import ProgressBar from "./progressBarWidget";
-import SkillSection from "./skillSection";
-export { SkillSection, ProgressBar, LabelList, NewlineText, OutlineBtn, BgBtn };
--- src/hook/index.js
+++
@@ -1,8 +0,0 @@
-import useBalance from "./useBalance";
-import useChainBalance from "./useChainBalance";
-import useCorrectChain from "./useCorrectChain";
-import useEn from "./useEn";
-import useScroll from "./useScroll";
-import useWeb3Modal from "./web3"
-import useWalletAddress from "./useWalletAddress";
-export {useWeb3Modal,useBalance, useChainBalance, useCorrectChain, useEn, useScroll, useWalletAddress}

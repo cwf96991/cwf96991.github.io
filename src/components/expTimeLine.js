a62de483640fabc821fba9dc0d1f0e35706b7405
cwf96991 <cwfflutter@gmail.com>
2023-05-16 09:33:35 -0400

    update resume

--- src/components/expTimeLine.js
+++ src/components/expTimeLine.js
@@ -11,18 +11,19 @@ const Roadmap = () => {
       subtitle: "VirgoCX Inc.",
       desc: "A mobile app provides crypto trading service and a marketplace to trade NFT",
       points:[
+        "-Implemented automated testing using Jest and Cypress to detect and resolve 80% of bugs prior to deployment, resulting in a 50% reduction in post-deployment bug fixes.",
         "-Reduced the bundle size by 30% by using the tracking dependencies tool.",
-        "-Reduced the time consumed in the whole CI/CD procedure by 20%.",
+        
       ]
     },
     {
       time: "May 2022 - Aug 2022",
-      title: "React Native Freelancer",
+      title: "Full Stack Developer",
       subtitle: "Kios",
       desc: "A mobile app that can interact with 3D objects in AR.",
       points:[
-        "-Embedded Unity 3D Model into a mobile application built with React Native.",
-        "-Built Interactable objects in the real world in AR environment.",
+        "-Implemented end-to-end authentication for a web application using Java Spring, resulting in a 50% reduction in unauthorized access attempts.",
+        "-Implemented continuous integration and deployment processes using Jenkins, reducing deployment time by 60% and increasing team productivity.",
         
       ]
     },
@@ -33,7 +34,7 @@ const Roadmap = () => {
       desc: "A mobile app provides crypto trading service and a marketplace to trade NFT",
       points:[
         "-Revamped the whole app and provide a better UI and UX to reach 1000+ daily users.",
-        "-Implemented the security feature which includes 2FA and biometric security checking.",
+        "-Collaborated with QA team to create and maintain a comprehensive suite of end-to-end tests, resulting in a 30% increase in test coverage and a decrease in overall defect rate by 20%.",
       ]
     },
     {
@@ -43,7 +44,7 @@ const Roadmap = () => {
       desc: "A mobile app provides academic support all-in-one platform",
       points:[
         "-Integrated API with a WebSocket server to build a real-time chatroom function for all online users.",
-        "-Implemented a login system that includes social login, such as Google,Facebook, and Apple"
+        "-Quickly learned new skills and applied them to daily tasks, improving efficiency and productivity."
       ]
     },
     {
